9f206f7a45cc33535ba94db9d5413adbb223de03
Kirill <daa_ma@mail.ru>
2024-03-11 22:50:13 +0300

    newMed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sys  # Импорт системного модуля
 
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
@@ -22,9 +23,50 @@ class MainWindow(QMainWindow):  # Определение класса MainWindow
 
         self.ui.pushButton.clicked.connect(self.openLoginForm)
 
+        self.conn = sqlite3.connect('patients_data.db')
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS table_data_4 (id INTEGER PRIMARY KEY, column1 TEXT, column2 TEXT, 
+            column3 TEXT, column4 TEXT, column5 TEXT, column6 TEXT, column7 TEXT, column8 TEXT)''')
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS table_data_5 (id INTEGER PRIMARY KEY, column1 TEXT, 
+            column2 TEXT, column3 TEXT, column4 TEXT, column5 TEXT, column6 TEXT, column7 TEXT, column8 TEXT)''')
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS table_data_6 (id INTEGER PRIMARY KEY, column1 TEXT, 
+            column2 TEXT, column3 TEXT, column4 TEXT, column5 TEXT, column6 TEXT, column7 TEXT, column8 TEXT)''')
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS table_data_7 (id INTEGER PRIMARY KEY, column1 TEXT, 
+            column2 TEXT, column3 TEXT, column4 TEXT, column5 TEXT, column6 TEXT, column7 TEXT, column8 TEXT)''')
+        self.conn.commit()
+
+    def save_table_data(self, tableWidget, table_name):
+        rowCount = tableWidget.rowCount()
+        columnCount = tableWidget.columnCount()
+        for row in range(rowCount):
+            row_data = []
+            for column in range(columnCount):
+                item = tableWidget.item(row, column)
+                if item is not None:  # Проверяем, есть ли элемент
+                    row_data.append(item.text())
+                else:
+                    row_data.append("")
+            # Сохраняем строку данных в базу данных
+            placeholders = ', '.join(['?'] * len(row_data))
+            self.cursor.execute(f"INSERT INTO {table_name} VALUES (NULL, {placeholders})", row_data)
+        self.conn.commit()
+
     def openLoginForm(self):
         self.login_form = Form()
         self.login_form.show()
+
+    def closeEvent(self, event):
+        self.save_table_data(self.ui.tableWidget_4, 'table_data_4')
+        self.save_table_data(self.ui.tableWidget_5, 'table_data_5')
+        self.save_table_data(self.ui.tableWidget_6, 'table_data_6')
+        self.save_table_data(self.ui.tableWidget_7, 'table_data_7')
+        self.conn.close()
+        super().closeEvent(event)
 
 
 if __name__ == "__main__":
